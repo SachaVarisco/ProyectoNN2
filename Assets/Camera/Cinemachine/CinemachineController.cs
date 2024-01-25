f98260483d758fb2b7edb28218a648b048d81942
MaxiRava <maximoravazzani@gmail.com>
2024-01-25 04:24:24 -0300

    Logs y zoom para el boss de la hormiga terminado

---
+++ Assets/Camera/Cinemachine/CinemachineController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachineController : MonoBehaviour
+{
+    
+    [SerializeField]
+    private CinemachineVirtualCamera vcam1;
+
+
+    [Header("ZoomAntBoss")]
+
+    [SerializeField] private float zoom;
+
+    private bool zoomActived = false;
+    private float velocity = 0f;
+    private float smoothTime = 0.25f;
+
+    void Update() {
+
+        if(zoomActived){
+
+            vcam1.m_Lens.OrthographicSize = Mathf.SmoothDamp(vcam1.m_Lens.OrthographicSize, zoom, ref velocity, smoothTime);
+        }
+        
+    }
+
+    public void ZoomAntBoss()
+    {
+        zoomActived = true;
+
+        //Debug.Log(zoomActived);
+    }
+
+
+}
